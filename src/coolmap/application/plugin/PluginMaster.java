3d202c9363cc5b2168d63272e17c6747cab0f76c
Keqiang Li <lkq@umich.edu>
2015-07-24 12:50:57 -0400

    fix passing the config json file issue

--- src/coolmap/application/plugin/PluginMaster.java
+++ src/coolmap/application/plugin/PluginMaster.java
@@ -7,7 +7,10 @@ package coolmap.application.plugin;
 
 import coolmap.application.widget.impl.console.CMConsole;
 import coolmap.utils.Config;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.net.URL;
 import java.util.Collection;
 import net.xeoh.plugins.base.PluginInformation;
 import net.xeoh.plugins.base.PluginManager;
@@ -24,16 +27,14 @@ public class PluginMaster {
 
     private static PluginManager pluginManager;
     private static PluginManagerUtil pluginManagerUtil;
-    
-    public Collection<CoolMapPlugin> getAllPlugins(){
-        if(pluginManagerUtil != null){
+
+    public Collection<CoolMapPlugin> getAllPlugins() {
+        if (pluginManagerUtil != null) {
             return pluginManagerUtil.getPlugins(CoolMapPlugin.class);
-        }
-        else{
+        } else {
             return null;
         }
     }
-    
 
     public static void initialize() {
         String pluginPath;
@@ -46,71 +47,84 @@ public class PluginMaster {
         File pluginFolder = new File(pluginPath);
 
         pluginManager = PluginManagerFactory.createPluginManager();
-            
-            File[] dirs = pluginFolder.listFiles();
-            for(File file : dirs){
-                if(file.isDirectory()){
-                    try{
-                        if (file.getName().endsWith(".plugin")) {
-                            pluginManager.addPluginsFrom(file.toURI());
-                        }
-                        
-                    }
-                    catch(Exception e){
-                        System.err.println("Error loading plugin from folder:" + file);
-                    }
-                }
-                else{
-                    //single jar will also work.
-                    try{
-                        if (file.getName().endsWith(".jar")) {
-                            pluginManager.addPluginsFrom(file.toURI());
-                        }
-                    }
-                    catch(Exception e){
-                        System.err.println("Error loading plugin from file:" + file);
-                    }
-                }
-            }    
 
-    //            after loading everything
-            pluginManagerUtil = new PluginManagerUtil(pluginManager);
-            Collection<CoolMapPlugin> plugins = pluginManagerUtil.getPlugins(CoolMapPlugin.class);
-
-            Collection<PluginInformation> pluginInfo = pluginManagerUtil.getPlugins(PluginInformation.class);
-
-            PluginInformationImpl piImpl = null;
-            if(!pluginInfo.isEmpty()){
-                piImpl = (PluginInformationImpl)pluginInfo.iterator().next();
-            }
-
-            for (CoolMapPlugin plugin : plugins) {
+        File[] dirs = pluginFolder.listFiles();
+        for (File file : dirs) {
+            if (file.isDirectory()) {
                 try {
-
-
-                    CMConsole.logInfo("Loaded " + plugin.getName() + " plugin");
-
-                    JSONObject config = new JSONObject();
-                    if(piImpl != null){
-                        //gets the string
-                        try{
-                        Collection<String> paths = piImpl.getInformation(PluginInformation.Information.CLASSPATH_ORIGIN, plugin);
-                        if(!paths.isEmpty()){
-                            config.put(CoolMapPluginTerms.ATTR_URI, paths.iterator().next());
-                        }}
-                        catch(Exception e){
-                            //do nothing, not found
-                        }
+                    if (file.getName().endsWith(".plugin")) {
+                        pluginManager.addPluginsFrom(file.toURI());
                     }
-
-                    plugin.initialize(config);
 
                 } catch (Exception e) {
-                    CMConsole.logWarning("Loaded '" + plugin.getName() + "' plugin");
-                    CMConsole.logToFile(e);
+                    System.err.println("Error loading plugin from folder:" + file);
+                }
+            } else {
+                //single jar will also work.
+                try {
+                    if (file.getName().endsWith(".jar")) {
+                        pluginManager.addPluginsFrom(file.toURI());
+                    }
+                } catch (Exception e) {
+                    System.err.println("Error loading plugin from file:" + file);
                 }
             }
-            
-            
         }
+
+        //            after loading everything
+        pluginManagerUtil = new PluginManagerUtil(pluginManager);
+        Collection<CoolMapPlugin> plugins = pluginManagerUtil.getPlugins(CoolMapPlugin.class);
+
+        Collection<PluginInformation> pluginInfo = pluginManagerUtil.getPlugins(PluginInformation.class);
+
+        PluginInformationImpl piImpl = null;
+        if (!pluginInfo.isEmpty()) {
+            piImpl = (PluginInformationImpl) pluginInfo.iterator().next();
+        }
+
+        for (CoolMapPlugin plugin : plugins) {
+            try {
+                CMConsole.logInfo("Loaded " + plugin.getName() + " plugin");
+
+                JSONObject config;
+                if (piImpl != null) {
+                    //gets the string
+                    try {
+                        Collection<String> paths = piImpl.getInformation(PluginInformation.Information.CLASSPATH_ORIGIN, plugin);
+
+                        if (!paths.isEmpty()) {
+                            String path = paths.iterator().next();
+                            
+                            if (path != null) {
+                                URL url = new URL(path);
+                                BufferedReader reader = new BufferedReader(new FileReader(url.getPath() + "config.json"));
+                                StringBuilder sb = new StringBuilder();
+                                String line;
+                                while ((line = reader.readLine()) != null) {
+                                    sb.append(line);
+                                }
+
+                                config = new JSONObject(sb.toString());
+                            } else {
+                                config = new JSONObject();
+                            }
+
+                            config.put(CoolMapPluginTerms.ATTR_URI, path);
+                        } else {
+                            config = new JSONObject();
+                        }
+                    } catch (Exception e) {
+                        config = new JSONObject();
+                    }
+                    
+                    plugin.initialize(config);
+                }
+
+            } catch (Exception e) {
+                CMConsole.logWarning("Loaded '" + plugin.getName() + "' plugin");
+                CMConsole.logToFile(e);
+            }
+        }
+
+    }
 }
